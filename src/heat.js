2ea8e7c61afdf47da4e1b9aaf50d70f00cae6a18
William Troup <williamtroup@gmail.com>
2024-01-09 22:10:37 +0000

    Added core JS layout.

--- src/heat.js
+++ src/heat.js
@@ -11,3 +11,345 @@
  */
 
 
+( function() {
+    var // Variables: Constructor Parameters
+        _parameter_Document = null,
+        _parameter_Window = null,
+
+        // Variables: Configuration
+        _configuration = {},
+
+        // Variables: Enums
+        _enum_KeyCodes = {
+            escape: 27
+        },
+
+        // Variables: Strings
+        _string = {
+            empty: "",
+            space: " "
+        },
+
+        // Variables: Elements
+        _elements_Type = {},
+
+        // Variables: Control Elements
+        _control_Elements = [],
+
+        // Variables: Attribute Names
+        _attribute_Name_Options = "data-heat-options";
+
+    
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Rendering
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function render() {
+        var tagTypes = _configuration.domElementTypes,
+            tagTypesLength = tagTypes.length;
+
+        for ( var tagTypeIndex = 0; tagTypeIndex < tagTypesLength; tagTypeIndex++ ) {
+            var domElements = _parameter_Document.getElementsByTagName( tagTypes[ tagTypeIndex ] ),
+                elements = [].slice.call( domElements ),
+                elementsLength = elements.length;
+
+            for ( var elementIndex = 0; elementIndex < elementsLength; elementIndex++ ) {
+                if ( !renderElement( elements[ elementIndex ] ) ) {
+                    break;
+                }
+            }
+        }
+    }
+
+    function renderElement( element ) {
+        var result = true;
+
+        if ( isDefined( element ) && element.hasAttribute( _attribute_Name_Options ) ) {
+            var bindingOptionsData = element.getAttribute( _attribute_Name_Options );
+
+            if ( isDefinedString( bindingOptionsData ) ) {
+                var bindingOptions = getObjectFromString( bindingOptionsData );
+
+                if ( bindingOptions.parsed && isDefinedObject( bindingOptions.result ) ) {
+                    bindingOptions = buildAttributeOptions( bindingOptions.result );
+
+                    element.removeAttribute( _attribute_Name_Options );
+
+                } else {
+                    if ( !_configuration.safeMode ) {
+                        console.error( "The attribute '" + _attribute_Name_Options + "' is not a valid object." );
+                        result = false;
+                    }
+                }
+
+            } else {
+                if ( !_configuration.safeMode ) {
+                    console.error( "The attribute '" + _attribute_Name_Options + "' has not been set correctly." );
+                    result = false;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Options
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function buildAttributeOptions( newOptions ) {
+        var options = !isDefinedObject( newOptions ) ? {} : newOptions;
+        options.render = getDefaultBoolean( options.render, true );
+
+        options = buildAttributeOptionStrings( options );
+
+        return buildAttributeOptionCustomTriggers( options );
+    }
+
+    function buildAttributeOptionStrings( options ) {
+        options.removeText = getDefaultString( options.removeText, "X" );
+
+        return options;
+    }
+
+    function buildAttributeOptionCustomTriggers( options ) {
+        options.onRenderComplete = getDefaultFunction( options.onRenderComplete, null );
+
+        return options;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Validation
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function isDefined( value ) {
+        return value !== null && value !== undefined && value !== _string.empty;
+    }
+
+    function isDefinedObject( object ) {
+        return isDefined( object ) && typeof object === "object";
+    }
+
+    function isDefinedBoolean( object ) {
+        return isDefined( object ) && typeof object === "boolean";
+    }
+
+    function isDefinedString( object ) {
+        return isDefined( object ) && typeof object === "string";
+    }
+
+    function isDefinedFunction( object ) {
+        return isDefined( object ) && typeof object === "function";
+    }
+
+    function isDefinedNumber( object ) {
+        return isDefined( object ) && typeof object === "number";
+    }
+
+    function isDefinedArray( object ) {
+        return isDefinedObject( object ) && object instanceof Array;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Element Handling
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function createElement( type, className ) {
+        var result = null,
+            nodeType = type.toLowerCase(),
+            isText = nodeType === "text";
+
+        if ( !_elements_Type.hasOwnProperty( nodeType ) ) {
+            _elements_Type[ nodeType ] = isText ? _parameter_Document.createTextNode( _string.empty ) : _parameter_Document.createElement( nodeType );
+        }
+
+        result = _elements_Type[ nodeType ].cloneNode( false );
+
+        if ( isDefined( className ) ) {
+            result.className = className;
+        }
+
+        return result;
+    }
+
+    function cancelBubble( e ) {
+        if ( e !== null ) {
+            e.preventDefault();
+            e.cancelBubble = true;
+        }
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Triggering Custom Events
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function fireCustomTrigger( triggerFunction ) {
+        if ( isDefinedFunction( triggerFunction ) ) {
+            triggerFunction.apply( null, [].slice.call( arguments, 1 ) );
+        }
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Default Parameter/Option Handling
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function getDefaultString( value, defaultValue ) {
+        return isDefinedString( value ) ? value : defaultValue;
+    }
+
+    function getDefaultBoolean( value, defaultValue ) {
+        return isDefinedBoolean( value ) ? value : defaultValue;
+    }
+
+    function getDefaultFunction( value, defaultValue ) {
+        return isDefinedFunction( value ) ? value : defaultValue;
+    }
+
+    function getDefaultArray( value, defaultValue ) {
+        return isDefinedArray( value ) ? value : defaultValue;
+    }
+
+    function getDefaultNumber( value, defaultValue ) {
+        return isDefinedNumber( value ) ? value : defaultValue;
+    }
+
+    function getDefaultStringOrArray( value, defaultValue ) {
+        if ( isDefinedString( value ) ) {
+            value = value.split( _string.space );
+
+            if ( value.length === 0 ) {
+                value = defaultValue;
+            }
+
+        } else {
+            value = getDefaultArray( value, defaultValue );
+        }
+
+        return value;
+    }
+
+    function getObjectFromString( objectString ) {
+        var parsed = true,
+            result = null;
+
+        try {
+            if ( isDefinedString( objectString ) ) {
+                result = JSON.parse( objectString );
+            }
+
+        } catch ( e1 ) {
+
+            try {
+                result = eval( "(" + objectString + ")" );
+
+                if ( isDefinedFunction( result ) ) {
+                    result = result();
+                }
+                
+            } catch ( e2 ) {
+                if ( !_configuration.safeMode ) {
+                    console.error( "Errors in object: " + e1.message + ", " + e2.message );
+                    parsed = false;
+                }
+                
+                result = null;
+            }
+        }
+
+        return {
+            parsed: parsed,
+            result: result
+        };
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Public Functions:  Configuration
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    /**
+     * setConfiguration().
+     * 
+     * Sets the specific configuration options that should be used.
+     * 
+     * @public
+     * 
+     * @param       {Options}   newConfiguration                            All the configuration options that should be set (refer to "Options" documentation for properties).
+     * 
+     * @returns     {Object}                                                The Heat.js class instance.
+     */
+    this.setConfiguration = function( newOptions ) {
+        _configuration = !isDefinedObject( newOptions ) ? {} : newOptions;
+        
+        buildDefaultConfiguration();
+
+        return this;
+    };
+
+    function buildDefaultConfiguration() {
+        _configuration.safeMode = getDefaultBoolean( _configuration.safeMode, true );
+        _configuration.domElementTypes = getDefaultStringOrArray( _configuration.domElementTypes, [ "*" ] );
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Public Functions:  Additional Data
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    /**
+     * getVersion().
+     * 
+     * Returns the version of Heat.js.
+     * 
+     * @public
+     * 
+     * @returns     {string}                                                The version number.
+     */
+    this.getVersion = function() {
+        return "0.1.0";
+    };
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Initialize Heat.js
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    ( function ( documentObject, windowObject ) {
+        _parameter_Document = documentObject;
+        _parameter_Window = windowObject;
+
+        buildDefaultConfiguration();
+
+        _parameter_Document.addEventListener( "DOMContentLoaded", function() {
+            render();
+        } );
+
+        if ( !isDefined( _parameter_Window.$heat ) ) {
+            _parameter_Window.$heat = this;
+        }
+
+    } ) ( document, window );
+} )();
