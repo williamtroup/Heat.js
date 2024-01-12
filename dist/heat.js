9fc10f846fc742e8a332c6395b845c2811444515
William Troup <williamtroup@gmail.com>
2024-01-12 23:45:31 +0000

    Updated to v0.6.0.

--- dist/heat.js
+++ dist/heat.js
@@ -1,4 +1,4 @@
-/*! Heat.js v0.5.0 | (c) Bunoon 2024 | MIT License */
+/*! Heat.js v0.6.0 | (c) Bunoon 2024 | MIT License */
 (function() {
   function w(a) {
     a.element.className = "heat-js";
@@ -346,7 +346,7 @@
     return this;
   };
   this.getVersion = function() {
-    return "0.5.0";
+    return "0.6.0";
   };
   (function(a, c) {
     C = a;
