9379a7ac530341812ae023a930e14c5fd3704c7e
William Troup <williamtroup@gmail.com>
2024-01-19 13:02:05 +0000

    Added the core translation for en.

---
+++ dist/translations/heat.translations.en.js
@@ -0,0 +1,38 @@
+/*! Heat.js v1.1.0 | (c) Bunoon 2024 | MIT License */
+$heat.setConfiguration( {
+    "stText": "st",
+    "ndText": "nd",
+    "rdText": "rd",
+    "thText": "th",
+    "backButtonText": "Back",
+    "nextButtonText": "Next",
+    "refreshButtonText": "Refresh",
+    "exportButtonText": "Export",
+    "lessText": "Less",
+    "moreText": "More",
+    "dateText": "Date",
+    "countText": "Count",
+    "monthNames": [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec"
+    ],
+    "dayNames": [
+            "Mon",
+            "Tue",
+            "Wed",
+            "Thu",
+            "Fri",
+            "Sat",
+            "Sun"
+    ]
+} );
