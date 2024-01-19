a5ebeb3136a79b89c29d9893b0e45ba8d3106594
William Troup <williamtroup@gmail.com>
2024-01-19 13:11:45 +0000

    Added es translation.

---
+++ dist/translations/heat.translations.es.js
@@ -0,0 +1,38 @@
+/*! Heat.js v1.1.0 | (c) Bunoon 2024 | MIT License */
+$heat.setConfiguration( {
+        "stText": "",
+        "ndText": "",
+        "rdText": "",
+        "thText": "",
+        "backButtonText": "Atrás",
+        "nextButtonText": "Próximo",
+        "refreshButtonText": "Actualizar",
+        "exportButtonText": "Exportar",
+        "lessText": "Menos",
+        "moreText": "Más",
+        "dateText": "Fecha",
+        "countText": "Contar",
+        "monthNames": [
+                "Ene",
+                "Feb",
+                "Mar",
+                "Abr",
+                "May",
+                "Jun",
+                "Jul",
+                "Ago",
+                "Sep",
+                "Oct",
+                "Nov",
+                "Dic"
+        ],
+        "dayNames": [
+                "Lun",
+                "Mar",
+                "Mie",
+                "Juev",
+                "Vie",
+                "Sab",
+                "Dom"
+        ]
+} );
