ccde7a219e541104730462e6173f2e459b978567
William Troup <williamtroup@gmail.com>
2024-01-19 13:22:27 +0000

    Added zh translations.

---
+++ dist/translations/heat.translations.zh.js
@@ -0,0 +1,38 @@
+/*! Heat.js v1.1.0 | (c) Bunoon 2024 | MIT License */
+$heat.setConfiguration( {
+        "stText": "",
+        "ndText": "",
+        "rdText": "",
+        "thText": "",
+        "backButtonText": "后退",
+        "nextButtonText": "下一个",
+        "refreshButtonText": "刷新",
+        "exportButtonText": "出口",
+        "lessText": "较少的",
+        "moreText": "更多的",
+        "dateText": "日期",
+        "countText": "数数",
+        "monthNames": [
+                "扬",
+                "二月",
+                "三月",
+                "四月",
+                "可能",
+                "君",
+                "七月",
+                "八月",
+                "九月",
+                "十月",
+                "十一月",
+                "十二月"
+        ],
+        "dayNames": [
+                "周一",
+                "星期二",
+                "星期三",
+                "星期四",
+                "周五",
+                "星期六",
+                "太阳"
+        ]
+} );
