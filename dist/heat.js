eaf14f28d61564494dfa5c7a559839dc2745fa33
William Troup <williamtroup@gmail.com>
2024-03-05 12:45:04 +0000

    Added new configuration option "objectErrorText", which states the error text that should be shown when an object error is detected (defaults to "Errors in object: {{error_1}}, {{error_2}}").

--- dist/heat.js
+++ dist/heat.js
@@ -1555,7 +1555,7 @@
         }
       } catch (e2) {
         if (!_configuration.safeMode) {
-          console.error("Errors in object: " + e1.message + ", " + e2.message);
+          console.error(_configuration.objectErrorText.replace("{{error_1}}", e1.message).replace("{{error_2}}", e2.message));
           parsed = false;
         }
         result = null;
@@ -1650,6 +1650,7 @@
     _configuration.unknownTrendText = getDefaultString(_configuration.unknownTrendText, "Unknown");
     _configuration.importButtonText = getDefaultString(_configuration.importButtonText, "Import");
     _configuration.noMapDataMessage = getDefaultString(_configuration.noMapDataMessage, "There is currently no data to view.");
+    _configuration.objectErrorText = getDefaultString(_configuration.objectErrorText, "Errors in object: {{error_1}}, {{error_2}}");
   }
   function buildDefaultConfigurationArrays() {
     if (isInvalidOptionArray(_configuration.monthNames, 12)) {
--- src/heat.js
+++ src/heat.js
@@ -2210,7 +2210,7 @@
                 
             } catch ( e2 ) {
                 if ( !_configuration.safeMode ) {
-                    console.error( "Errors in object: " + e1.message + ", " + e2.message );
+                    console.error( _configuration.objectErrorText.replace( "{{error_1}}",  e1.message ).replace( "{{error_2}}",  e2.message ) );
                     parsed = false;
                 }
                 
@@ -3169,6 +3169,7 @@
         _configuration.unknownTrendText = getDefaultString( _configuration.unknownTrendText, "Unknown" );
         _configuration.importButtonText = getDefaultString( _configuration.importButtonText, "Import" );
         _configuration.noMapDataMessage = getDefaultString( _configuration.noMapDataMessage, "There is currently no data to view." );
+        _configuration.objectErrorText = getDefaultString( _configuration.objectErrorText, "Errors in object: {{error_1}}, {{error_2}}" );
     }
 
     function buildDefaultConfigurationArrays() {
