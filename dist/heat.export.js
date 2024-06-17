e4a492b422ccda68715e79cb4be4853c6bf1b152
William Troup <williamtroup@gmail.com>
2024-06-17 10:50:30 +0100

    Added export support for the "$heat" object, which can now be imported as "heat.js".

---
+++ dist/heat.export.js
@@ -0,0 +1,6 @@
+/*! Heat.js v3.2.0 | (c) Bunoon 2024 | MIT License */
+var heat = {js:function() {
+  return window.$heat;
+}};
+Object.assign(window, {heat});
+export{heat};
---
+++ src/heat.export.js
@@ -0,0 +1,22 @@
+/**
+ * Heat.js
+ * 
+ * A lightweight JavaScript library that generates customizable heat maps, charts, and statistics to visualize date-based activity and trends.
+ * 
+ * @file        observe.js
+ * @version     v3.2.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+var heat = {
+    js: function() {
+        return window.$heat;
+    }
+};
+
+Object.assign( window, { heat } );
+
+export { heat };
