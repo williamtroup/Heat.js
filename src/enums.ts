f534cba80feab213b98cff0a5b03b35191be1109
William Troup <williamtroup@gmail.com>
2024-06-28 11:27:07 +0100

    Added enums.

---
+++ src/enums.ts
@@ -0,0 +1,36 @@
+export enum STRING {
+    empty = "",
+    space = " ",
+    newLine = "\n",
+    dash = "-",
+    underscore = "_",
+    plus = "+",
+    zero = "0",
+    colon = ":",
+    comma = ","
+};
+
+export enum VALUE {
+    notFound = -1
+};
+
+export enum VIEW {
+    map = 1,
+    chart = 2,
+    days = 3,
+    statistics = 4
+};
+
+export enum VIEW_NAME {
+    map = "map",
+    chart = "chart",
+    days = "days",
+    statistics = "statistics"
+};
+
+export enum EXPORT_TYPE {
+    csv = "csv",
+    json = "json",
+    xml = "xml",
+    txt = "txt"
+}
--- src/heat.ts
+++ src/heat.ts
@@ -1,1 +1,2 @@
 import { type Configuration, type Holiday, type ColorRange, type BindingOptions } from "./types";
+import { STRING, VALUE, VIEW, VIEW_NAME, EXPORT_TYPE } from "./enums";
