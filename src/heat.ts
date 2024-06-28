c69cfce006d9d9c0d20e058af841abbdd8c01605
William Troup <williamtroup@gmail.com>
2024-06-28 11:59:00 +0100

    Added initialise function.

--- src/heat.ts
+++ src/heat.ts
@@ -1,3 +1,7 @@
 import { type Configuration, type Holiday, type ColorRange, type BindingOptions } from "./types";
 import { STRING, VALUE, VIEW, VIEW_NAME, EXPORT_TYPE } from "./enums";
 import { type PublicApi } from "./api";
+
+(() => {
+    
+})();
