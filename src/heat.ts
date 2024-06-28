70ef87abc24b620b8f64dddc46bda50ed8671c1c
William Troup <williamtroup@gmail.com>
2024-06-28 12:08:21 +0100

    Added initialiser.

--- src/heat.ts
+++ src/heat.ts
@@ -2,6 +2,18 @@ import { type Configuration, type Holiday, type ColorRange, type BindingOptions 
 import { STRING, VALUE, VIEW, VIEW_NAME, EXPORT_TYPE } from "./enums";
 import { type PublicApi } from "./api";
 
-(() => {
-    
-})();
+(( documentObject, windowObject, mathObject, jsonObject ) => {
+
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Initialize Heat.js
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    (() => {
+
+    })();
+
+})( document, window, Math, JSON );
