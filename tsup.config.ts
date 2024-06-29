daa464973a417a7beb9f97282b87f9b6f4d6385d
William Troup <williamtroup@gmail.com>
2024-06-29 21:00:25 +0100

    Config updates.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,14 +2,19 @@ import { defineConfig } from "tsup";
 
 export default defineConfig([
   {
-    entry: ["src/heat.ts"],
+    entry: [
+      "src/heat.ts"
+    ],
     platform: "neutral",
-    format: ["esm", "cjs"],
+    format: [
+      "esm",
+      "cjs"
+    ],
     outDir: "./dist",
     dts: true,
     splitting: false,
     sourcemap: true,
     minify: false,
-    clean: false,
-  },
+    clean: false
+  }
 ]);
