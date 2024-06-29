4e23a58c31c81206b2dbe92493bcadc60a06d474
William Troup <williamtroup@gmail.com>
2024-06-29 20:27:12 +0100

    New build configuration.

--- dist/ts/enum.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=enum.js.map
---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    entry: ["src/heat.ts"],
+    platform: "neutral",
+    format: ["esm", "cjs"],
+    outDir: "./dist",
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    minify: false,
+    clean: false,
+  },
+]);
