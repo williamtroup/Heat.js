23a62335fcbcd545a5acd1b2e3172f483e4dbcd4
William Troup <williamtroup@gmail.com>
2025-11-28 10:08:44 +0000

    Updated to v4.5.1.

--- src/heat.ts
+++ src/heat.ts
@@ -4,7 +4,7 @@
  * A lightweight JavaScript library that generates customizable heat maps, charts, and statistics to visualize date-based activity and trends.
  * 
  * @file        heat.ts
- * @version     v4.5.0
+ * @version     v4.5.1
  * @author      Bunoon
  * @license     MIT License
  * @copyright   Bunoon 2025
@@ -3000,7 +3000,7 @@ type LargestValuesForEachRangeType = {
         },
 
         getVersion: function () : string {
-            return "4.5.0";
+            return "4.5.1";
         }
     };
 
