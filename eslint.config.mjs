e0d90cad5adb04be068786cec8a5903334a71bc2
William Troup <williamtroup@gmail.com>
2025-12-31 00:32:28 +0000

    Added no-debugger to eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,6 +37,7 @@ export default defineConfig( [ {
         "@typescript-eslint/no-namespace": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unsafe-function-type": "off",
+        "no-debugger": "off",
         "prefer-spread": "off",
     },
 } ] );
