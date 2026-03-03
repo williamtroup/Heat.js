4de79396b11ea7685341a63aeebbdd9da67e23f5
William Troup <williamtroup@gmail.com>
2026-03-03 22:11:01 +0000

    Minor refactor.

--- src/ts/area/chart.ts
+++ src/ts/area/chart.ts
@@ -23,30 +23,30 @@ export namespace Chart {
             if ( largestValue > 0 ) {
                 const valueIncrease: number = largestValue / ( maximumLabels - 1 );
                 const positionIncrease: number = 100 / ( maximumLabels - 1 );
-                let topLabel: HTMLElement;
+                let topYLabel: HTMLElement;
 
                 for ( let labelIndex = 0; labelIndex < maximumLabels; labelIndex++ ) {
-                    const newLabel: HTMLElement = DomElement.create( yAxisLabels, "div", "chart-y-label" );
+                    const newYLabel: HTMLElement = DomElement.create( yAxisLabels, "div", "chart-y-label" );
 
                     if ( labelIndex === 0 ) {
-                        newLabel.innerHTML = Char.zero;
-                        newLabel.style.bottom = "0";
-                        newLabel.style.transform = "translateY( 50% )";
+                        newYLabel.innerHTML = Char.zero;
+                        newYLabel.style.bottom = "0";
+                        newYLabel.style.transform = "translateY( 50% )";
 
                     } else if ( labelIndex === maximumLabels - 1 ) {
-                        newLabel.innerHTML = largestValue.toString();
-                        newLabel.style.top = "0";
-                        newLabel.style.transform = "translateY( -50% )";
-                        topLabel = newLabel;
+                        newYLabel.innerHTML = largestValue.toString();
+                        newYLabel.style.top = "0";
+                        newYLabel.style.transform = "translateY( -50% )";
+                        topYLabel = newYLabel;
 
                     } else {
-                        newLabel.innerHTML = Math.floor( valueIncrease * labelIndex ).toString();
-                        newLabel.style.top = `${100 - ( positionIncrease * labelIndex )}%`;
-                        newLabel.style.transform = "translateY( -50% )";
+                        newYLabel.innerHTML = Math.floor( valueIncrease * labelIndex ).toString();
+                        newYLabel.style.top = `${100 - ( positionIncrease * labelIndex )}%`;
+                        newYLabel.style.transform = "translateY( -50% )";
                     }
                 }
 
-                yAxisLabels.style.width = `${topLabel!.offsetWidth}px`;
+                yAxisLabels.style.width = `${topYLabel!.offsetWidth}px`;
                 labelsWidth = yAxisLabels.offsetWidth;
 
             } else {
@@ -63,14 +63,14 @@ export namespace Chart {
                 const positionIncrease: number = 100 / ( maximumLabels - 1 );
 
                 for ( let labelIndex = 1; labelIndex < maximumLabels; labelIndex++ ) {
-                    const newLine: HTMLElement = DomElement.create( lines, "span", "chart-y-line" );
+                    const newYLine: HTMLElement = DomElement.create( lines, "span", "chart-y-line" );
 
                     if ( labelIndex === maximumLabels - 1 ) {
-                        newLine.style.top = "0";
+                        newYLine.style.top = "0";
 
                     } else {
-                        newLine.style.top = `${100 - ( positionIncrease * labelIndex )}%`;
-                        newLine.style.transform = "translateY( -50% )";
+                        newYLine.style.top = `${100 - ( positionIncrease * labelIndex )}%`;
+                        newYLine.style.transform = "translateY( -50% )";
                     }
                 }
             }
